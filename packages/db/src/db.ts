746f7740644dd9c7e347ecf9724c157b6bf36c20
Saad Haider <mohd.saadhaider@gmail.com>
2026-08-15 17:39:44 +0530

    fix: force IPv4 for db connections

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,4 +1,12 @@
 import mongoose, { type ConnectOptions } from "mongoose";
+import dns from "node:dns";
+
+// Mongo doesn't support ipv6 tcp connections and standalone
+// node process running on node 17+ uses ipv6 by default
+// So we prefer ipv4 DNS lookups across all environments
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder("ipv4first");
+}
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
@@ -15,8 +23,13 @@ if (!cached) {
 }
 
 async function connectDB() {
-  if (cached.conn) {
+  if (cached.conn && cached.conn.connection?.readyState === 1) {
     return cached.conn;
+  }
+
+  if (cached.conn && cached.conn.connection?.readyState === 0) {
+    cached.conn = null;
+    cached.promise = null;
   }
 
   if (!cached.promise) {
@@ -26,13 +39,14 @@ async function connectDB() {
       minPoolSize: 1,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 0,
+      family: 4, // forces mongo driver to req ipv4 addresses
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
-  
+
   try {
     cached.conn = await cached.promise;
   } catch (e) {
